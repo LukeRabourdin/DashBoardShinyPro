744eda8d3be0278266397e512ddd18b08512953b
LukeRabourdin <93909516+LukeRabourdin@users.noreply.github.com>
2026-02-26 09:47:03 +0100

    Fix bottom clipping feel with balanced vertical map framing

--- Dashboard/FonctionsDashboard.R
+++ Dashboard/FonctionsDashboard.R
@@ -2446,7 +2446,7 @@ france_map_kpi_server <- function(id, data_r) {
           xspan <- max(1, as.numeric(bb["xmax"] - bb["xmin"]))
           yspan <- max(1, as.numeric(bb["ymax"] - bb["ymin"]))
           pad_x <- xspan * 0.132
-          pad_y <- yspan * 0.132
+          pad_y <- yspan * 0.16
 
           view_bb <- c(
             xmin = as.numeric(bb["xmin"] - pad_x),
@@ -2479,7 +2479,7 @@ france_map_kpi_server <- function(id, data_r) {
         fill_col[mask] <- pal[idx[mask]]
 
         par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
-        plot(sf::st_geometry(map_view), col = fill_col, border = "#FFFFFF", lwd = 0.6, asp = NA)
+        plot(sf::st_geometry(map_view), col = fill_col, border = "#FFFFFF", lwd = 0.6, asp = 1)
         plot(sf::st_geometry(target_ops), add = TRUE, border = "#5A46B8", lwd = 2.1)
 
         label_df <- map_ops[map_ops$zone %in% c("Cible", "Limitrophes") & is.finite(map_ops$value), ]
